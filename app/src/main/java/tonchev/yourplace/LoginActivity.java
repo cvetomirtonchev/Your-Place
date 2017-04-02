cdbc0432780235456c79a2080eea6467be043660
Tsvetomir Tonchev <cvetomirtonchev@gmail.com>
2017-04-03 00:24:38 +0300

    google button not working

--- app/src/main/java/tonchev/yourplace/LoginActivity.java
+++ app/src/main/java/tonchev/yourplace/LoginActivity.java
@@ -1,13 +1,29 @@
 package tonchev.yourplace;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private SignInButton signInButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+
+
     }
+
 }
