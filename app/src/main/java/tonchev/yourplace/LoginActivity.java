2cb05deeec093ec79836786d86ec993788c651d1
Tsvetomir Tonchev <cvetomirtonchev@gmail.com>
2017-04-03 20:00:17 +0300

    s

--- app/src/main/java/tonchev/yourplace/LoginActivity.java
+++ app/src/main/java/tonchev/yourplace/LoginActivity.java
@@ -1,12 +1,16 @@
 package tonchev.yourplace;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
