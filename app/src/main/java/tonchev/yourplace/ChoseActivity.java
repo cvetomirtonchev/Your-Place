0b7b033d47420cc0888d231275f3283dfd3f15a8
Tsvetomir Tonchev <cvetomirtonchev@gmail.com>
2017-04-06 15:00:44 +0300

    added toolbar

--- app/src/main/java/tonchev/yourplace/ChoseActivity.java
+++ app/src/main/java/tonchev/yourplace/ChoseActivity.java
@@ -1,6 +1,9 @@
 package tonchev.yourplace;
 
 import android.app.Activity;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.design.widget.TabLayout;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.os.Bundle;
@@ -15,20 +18,27 @@ public class ChoseActivity extends AppCompatActivity {
     private DrawerLayout nDrawerLayoun;
     private ActionBarDrawerToggle mTogle;
     private Toolbar nToolBar;
+    private TabLayout nTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chose);
+        // Toolbar and navigation drawer
         nToolBar = (Toolbar) findViewById(R.id.navigation_action);
-
         setSupportActionBar(nToolBar);
-
         nDrawerLayoun = (DrawerLayout) findViewById(R.id.drawer_layout);
         mTogle = new ActionBarDrawerToggle(this,nDrawerLayoun,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         nDrawerLayoun.addDrawerListener(mTogle);
         mTogle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //TabLayout
+        nTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        nTabLayout.addTab(nTabLayout.newTab().setText("Pick"));
+        nTabLayout.addTab(nTabLayout.newTab().setText("Map"));
+
+
+
 
 
 
