d7fc354dc21cb025681e6bfedb63875e87dfbdf8
Vasil Goranov <vasil.goranov@gmail.com>
2017-04-10 12:42:55 +0300

    search opit

--- app/src/main/java/tonchev/yourplace/LoginActivity.java
+++ app/src/main/java/tonchev/yourplace/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -44,6 +45,16 @@ public class LoginActivity extends AppCompatActivity implements OnConnectionFail
             @Override
             public void onClick(View v) {
                   signIn();
+            }
+        });
+
+        Button opit;
+        opit = (Button) findViewById(R.id.opit);
+        opit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, OpitActivity.class);
+                startActivity(intent);
             }
         });
 
---
+++ app/src/main/java/tonchev/yourplace/OpitActivity.java
@@ -0,0 +1,46 @@
+package tonchev.yourplace;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+
+public class OpitActivity extends AppCompatActivity {
+
+    private TextView tv;
+    private ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opit);
+        img = (ImageView) findViewById(R.id.place_img);
+        tv = (TextView) findViewById(R.id.text_name);
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                tv.setText(place.getName());
+            }
+
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Toast.makeText(OpitActivity.this, "An error occurred: " + status, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+}
